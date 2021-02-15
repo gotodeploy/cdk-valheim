297e0f3b6098be9ea0f97d0de3f7e8e5e8939634
Neil Guan <guan840912@gmail.com>
2021-02-15 15:59:20 +0800

    Give cpu memory detailed instructions
    new logGroup for Custom logGroup Name

--- src/valheim.ts
+++ src/valheim.ts
@@ -2,13 +2,46 @@ import * as ec2 from '@aws-cdk/aws-ec2';
 import * as ecs from '@aws-cdk/aws-ecs';
 import * as efs from '@aws-cdk/aws-efs';
 import * as logs from '@aws-cdk/aws-logs';
-import { CfnOutput, Construct } from '@aws-cdk/core';
+import { CfnOutput, Construct, RemovalPolicy } from '@aws-cdk/core';
 
 export interface ValheimWorldProps {
   readonly vpc?: ec2.IVpc;
   readonly fileSystem?: efs.FileSystem;
   readonly image?: ecs.ContainerImage;
+  /**
+   * The number of cpu units used by the task. For tasks using the Fargate launch type,
+   * this field is required and you must use one of the following values,
+   * which determines your range of valid values for the memory parameter:
+   *
+   * 256 (.25 vCPU) - Available memory values: 512 (0.5 GB), 1024 (1 GB), 2048 (2 GB)
+   *
+   * 512 (.5 vCPU) - Available memory values: 1024 (1 GB), 2048 (2 GB), 3072 (3 GB), 4096 (4 GB)
+   *
+   * 1024 (1 vCPU) - Available memory values: 2048 (2 GB), 3072 (3 GB), 4096 (4 GB), 5120 (5 GB), 6144 (6 GB), 7168 (7 GB), 8192 (8 GB)
+   *
+   * 2048 (2 vCPU) - Available memory values: Between 4096 (4 GB) and 16384 (16 GB) in increments of 1024 (1 GB)
+   *
+   * 4096 (4 vCPU) - Available memory values: Between 8192 (8 GB) and 30720 (30 GB) in increments of 1024 (1 GB)
+   *
+   * @default 512
+   */
   readonly cpu?: number;
+  /**
+   * The amount (in MiB) of memory used by the task. For tasks using the Fargate launch type,
+   * this field is required and you must use one of the following values, which determines your range of valid values for the cpu parameter:
+   *
+   * 512 (0.5 GB), 1024 (1 GB), 2048 (2 GB) - Available cpu values: 256 (.25 vCPU)
+   *
+   * 1024 (1 GB), 2048 (2 GB), 3072 (3 GB), 4096 (4 GB) - Available cpu values: 512 (.5 vCPU)
+   *
+   * 2048 (2 GB), 3072 (3 GB), 4096 (4 GB), 5120 (5 GB), 6144 (6 GB), 7168 (7 GB), 8192 (8 GB) - Available cpu values: 1024 (1 vCPU)
+   *
+   * Between 4096 (4 GB) and 16384 (16 GB) in increments of 1024 (1 GB) - Available cpu values: 2048 (2 vCPU)
+   *
+   * Between 8192 (8 GB) and 30720 (30 GB) in increments of 1024 (1 GB) - Available cpu values: 4096 (4 vCPU)
+   *
+   * @default 2048
+   */
   readonly memoryLimitMiB?: number;
   readonly desiredCount?: number;
 
@@ -18,6 +51,12 @@ export interface ValheimWorldProps {
   readonly environment?: {
     [key: string]: string;
   };
+  /**
+   * valheim server log Group RemovalPolicy.
+   *
+   * @default - RemovalPolicy.DESTROY
+   */
+  readonly logGroupRemovalPolicy?: RemovalPolicy;
 }
 
 
@@ -45,7 +84,12 @@ export class ValheimWorld extends Construct {
         fileSystemId: fileSystem.fileSystemId,
       },
     };
-
+    // Create the logGroup.
+    const logGroup = new logs.LogGroup(this, 'ValheimServer', {
+      logGroupName: 'ValheimServer',
+      retention: logs.RetentionDays.ONE_DAY,
+      removalPolicy: props?.logGroupRemovalPolicy ?? RemovalPolicy.DESTROY,
+    } );
     const taskDefinition = new ecs.FargateTaskDefinition(this, 'ValheimTaskDefinition', {
       family: 'valheim-world',
       volumes: [volumeConfig],
@@ -57,7 +101,7 @@ export class ValheimWorld extends Construct {
       image: props?.image ?? ecs.ContainerImage.fromRegistry('lloesche/valheim-server'),
       logging: new ecs.AwsLogDriver({
         streamPrefix: 'valheim',
-        logRetention: logs.RetentionDays.ONE_DAY,
+        logGroup: logGroup,
       }),
       environment: props?.environment,
     });
