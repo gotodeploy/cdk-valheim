bbbe2d7cb32d587da90f94603b9f0ae821405d86
gotodeploy <1491134+gotodeploy@users.noreply.github.com>
2021-03-05 00:02:51 +0900

    chore(deps): bump projen from 1.90.1 to 1.91.0

--- .projenrc.js
+++ .projenrc.js
@@ -3,7 +3,7 @@ const { AwsCdkConstructLibrary } = require('projen');
 const project = new AwsCdkConstructLibrary({
   author: 'gotodeploy',
   authorAddress: '1491134+gotodeploy@users.noreply.github.com',
-  cdkVersion: '1.90.1',
+  cdkVersion: '1.91.0',
   defaultReleaseBranch: 'main',
   jsiiFqn: 'projen.AwsCdkConstructLibrary',
   name: 'cdk-valheim',
